f0f34b7e9cfaa9374f4ae39aa7ae3a1c64b535ca
YuJeSeon <dbwptjs625@gmail.com>
2023-06-14 00:37:06 +0900

    Refactoring:batchsize

--- server/src/main/java/application/Seb43Main029Application.java
+++ server/src/main/java/application/Seb43Main029Application.java
@@ -2,10 +2,12 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class Seb43Main029Application {
 
 	public static void main(String[] args) {
